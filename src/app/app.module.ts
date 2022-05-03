f05be5068943f1b8d1e943df50588e68b1701dd0
Lucas Labre <lucaslabre@gmail.com>
2022-05-03 17:58:45 -0300

    Método CREATE implementado para Categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -15,9 +19,9 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { HomeComponent } from './components/views/home/home.component';
-
 
 
 @NgModule({
@@ -27,7 +31,8 @@ import { HomeComponent } from './components/views/home/home.component';
     FooterComponent,
     NavComponent,
     HomeComponent,
-    CategoriaReadComponent
+    CategoriaReadComponent,
+    CategoriaCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +45,11 @@ import { HomeComponent } from './components/views/home/home.component';
     MatCardModule,
     MatTableModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/views/categoria/categoria-read/categoria-read.component.ts
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoriaService } from '../categoria.service';
 import { Categoria } from '../categoria.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categoria-read',
@@ -13,17 +14,24 @@ export class CategoriaReadComponent implements OnInit {
   
   displayedColumns: string[] = ['id', 'nome', 'descricao', 'livros', 'acoes'];
 
-  constructor(private service: CategoriaService) { }
+  constructor(
+    private service: CategoriaService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.findAll();
   }
 
-  findAll() {
+  findAll(): void {
     this.service.findAll().subscribe(response => {
       console.log(response);
       this.categorias = response;
     })
+  }
+
+  navegarParaCategoriaCreate(): void {
+    this.router.navigate(["categorias/create"])
   }
 
 }
